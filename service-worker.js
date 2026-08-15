370132438df16816f96b5c6a3e19538180ae1f34
KHALED SABRY <khaledsabry255@gmail.com>
2026-08-15 21:47:24 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "ncm-permits-cache-v2";
-const CORE_ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+const CACHE_NAME = "ncm-permits-cache-v3";
+const CORE_ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
